bc500c902d39a3b995f659cdd890f5c88975c221
Thiago <91092364+thag0@users.noreply.github.com>
2023-05-21 23:06:01 -0300

    Add files via upload

--- RedeNeural.c
+++ RedeNeural.c
@@ -2,6 +2,7 @@
 #include <math.h>
 
 #define BIAS 1
+#define TAXA_APRENDIZAGEM 0.2
 
 #define FUNCAO_ATIVACAO(valor) RNA_funcao_ativacao_relu(valor)
 #define FUNCAO_ATIVACAO_SAIDA(valor) RNA_funcao_ativacao_reluDx(valor)
@@ -31,15 +32,21 @@ typedef struct{
 
 RedeNeural* RNA_criar_rede(int qtd_neuronios_entrada, int qtd_neuronios_escondida, int qtd_neuronios_saida, int qtd_camadas_escondidas);
 void RNA_apagar_rede(RedeNeural* rede);
+void RNA_inicializar_neuronio(Neuronio* neuronio, int quantidade_ligacoes);
+
 double RNA_funcao_ativacao_relu(double valor);
 double RNA_funcao_ativacao_reluDx(double valor);
 double RNA_funcao_ativacao_sigmoid(double valor);
-void RNA_inicializar_neuronio(Neuronio* neuronio, int quantidade_ligacoes);
-void RNA_imprimir_pesos_entrada(RedeNeural* rede);
+
+
 void RNA_calcular_entrada(RedeNeural* rede, double dados[]);
-void RNA_imprimir_saida_neuronio_saida(RedeNeural* rede);
+void RNA_treinar(RedeNeural* rede, double entrada[], double alvo[]);
 
+void RNA_imprimir_saidas_entrada(RedeNeural* rede);
+void RNA_imprimir_saidas_ocultas(RedeNeural* rede);
+void RNA_imprimir_saidas_saida(RedeNeural* rede);
 
+//------------------------------------------------------------------
 double RNA_funcao_ativacao_relu(double valor){
    if(valor < 0) return 0;
    else return valor;
@@ -170,6 +177,60 @@ void RNA_calcular_entrada(RedeNeural* rede, double dados[]){
 }
 
 
+void RNA_treinar(RedeNeural* rede, double entrada[], double alvo[]){
+   double erro_total = 0.0;
+
+   // Etapa de propagação direta (forward propagation)
+   RNA_calcular_entrada(rede, entrada);
+
+   // Cálculo do erro na camada de saída
+   for(int i = 0; i < rede->saida.quantidade_neuronios; i++){
+      double saida = rede->saida.neuronios[i].saida;
+      double erro = alvo[i] - saida;
+      erro_total += pow(erro, 2);
+
+      // Cálculo do gradiente local na camada de saída
+      double gradiente_local = erro * FUNCAO_ATIVACAO(saida);
+
+      // Atualização dos pesos na camada de saída
+      for(int j = 0; j < rede->quantidade_oculta; j++){
+         Camada* oculta = &rede->ocultas[rede->quantidade_oculta - 1];
+         double saida_oculta = oculta->neuronios[j].saida;
+         double delta_peso = gradiente_local * saida_oculta * TAXA_APRENDIZAGEM;
+         rede->saida.neuronios[i].peso[j] += delta_peso;
+      }
+   }
+
+   // Cálculo do erro nas camadas ocultas e atualização dos pesos
+   for(int i = rede->quantidade_oculta - 1; i >= 0; i--){
+      Camada* camada_atual = &rede->ocultas[i];
+      Camada* camada_anterior;
+
+      if(i == 0) camada_anterior = &rede->entrada;
+      else camada_anterior = &rede->ocultas[i - 1];
+
+      for(int j = 0; j < camada_atual->quantidade_neuronios; j++){
+         double gradiente_local = 0.0;
+
+         // Cálculo do gradiente local na camada oculta
+         for(int k = 0; k < rede->saida.quantidade_neuronios; k++){
+            double erro = alvo[k] - rede->saida.neuronios[k].saida;
+            double peso = rede->saida.neuronios[k].peso[j];
+            gradiente_local += erro * FUNCAO_ATIVACAO_SAIDA(rede->saida.neuronios[k].saida) * peso;
+         }
+
+         // Atualização dos pesos na camada oculta
+         for(int l = 0; l < camada_anterior->quantidade_neuronios; l++){
+            double saida_anterior = camada_anterior->neuronios[l].saida;
+            double delta_peso = gradiente_local * saida_anterior * TAXA_APRENDIZAGEM;
+            camada_atual->neuronios[j].peso[l] += delta_peso;
+         }
+      }
+   }
+}
+
+
+//impressões
 void RNA_imprimir_saidas_entrada(RedeNeural* rede){
    printf("--Entrada--\n");
    for(int i = 0; i < rede->entrada.quantidade_neuronios; i++){
--- main.c
+++ main.c
@@ -2,33 +2,93 @@
 #include <time.h>
 #include "RedeNeural.c"
 
+#define QTD_TREINO 10000
+
 int main(){
    srand(time(NULL));
    system("cls");
 
-   int qtd_neuronios_entrada = 3;
-   int qtd_neuronios_escondida = 4;
-   int qtd_neuronios_saida = 3;
-   int qtd_camadas_ocultas = 1;
+   int qtd_neuronios_entrada = 2;
+   int qtd_neuronios_oculta = 3;
+   int qtd_neuronios_saida = 1;
+   int qtd_camadas_ocultas = 2;
 
-   double dados[3] = {1.2, 3.4, 5.6};
+   int linhas_dados = 4;
+   int colunas_dados = 3;
+   double dados[4][3] = { 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0};
 
-   RedeNeural* rede = RNA_criar_rede(qtd_neuronios_entrada, qtd_neuronios_escondida, qtd_neuronios_saida, qtd_camadas_ocultas);
-   RedeNeural* rede2 = RNA_criar_rede(qtd_neuronios_entrada, qtd_neuronios_escondida, qtd_neuronios_saida, qtd_camadas_ocultas);
+   double* classe = (double*) malloc(sizeof(double));
+   double* entrada_teste = (double*) malloc(2 * sizeof(double));
+   double dados_predict[] = {0, 1};
+
+   RedeNeural* rede = RNA_criar_rede(qtd_neuronios_entrada, qtd_neuronios_oculta, qtd_neuronios_saida, qtd_camadas_ocultas);
    //implementar treino da rede
-   printf("rede 1\n");
-   RNA_calcular_entrada(rede, dados);
+   printf("-Antes do treino-\n");
+   RNA_calcular_entrada(rede, dados_predict);
    RNA_imprimir_saidas_entrada(rede);
    RNA_imprimir_saidas_ocultas(rede);
    RNA_imprimir_saidas_saida(rede);
-   RNA_apagar_rede(rede);
 
-   printf("rede 2\n");
-   RNA_calcular_entrada(rede2, dados);
-   RNA_imprimir_saidas_entrada(rede2);
-   RNA_imprimir_saidas_ocultas(rede2);
-   RNA_imprimir_saidas_saida(rede2);
-   RNA_apagar_rede(rede2);
+   for(int i = 0; i < QTD_TREINO; i++){
+      if(i%4 == 0){
+         entrada_teste[0] = dados[0][0];
+         entrada_teste[1] = dados[0][1];
+         *classe = dados[0][2];
+
+      }else if(i%3 == 0){
+         entrada_teste[0] = dados[1][0];
+         entrada_teste[1] = dados[1][1];
+         *classe = dados[1][2];
+
+      }else if(i%2 == 0){
+         entrada_teste[0] = dados[2][0];
+         entrada_teste[1] = dados[2][1];
+         *classe = dados[2][2];
+      }else{
+         entrada_teste[0] = dados[3][0];
+         entrada_teste[1] = dados[3][1];
+         *classe = dados[3][2];
+      }
+
+
+      RNA_treinar(rede, entrada_teste, classe);
+   }
+
+   printf("-Depois do treino-\n");
+   RNA_calcular_entrada(rede, dados_predict);
+   RNA_imprimir_saidas_entrada(rede);
+   RNA_imprimir_saidas_ocultas(rede);
+   RNA_imprimir_saidas_saida(rede);
+
+   char resposta = ' ';
+   printf("Fazer uma predicao ? [s/n]: ");
+   scanf(" %c", &resposta);
+   if(resposta == 's' || resposta == 'S'){
+      int loop = 1;
+      while(loop == 1){
+         system("cls");
+         printf("\t-Predicao-\n");
+         printf("valor 00: ");
+         scanf("%lf", &dados_predict[0]);
+         
+         printf("valor 01: ");
+         scanf("%lf", &dados_predict[1]);
+
+         RNA_calcular_entrada(rede, dados_predict);
+         RNA_imprimir_saidas_saida(rede);
+
+
+         printf("\nDigite qualquer coisa para continuar ou x para sair: ");
+         scanf(" %c", &resposta);
+         if(resposta == 'x' || resposta == 'X') loop = 0;
+      }
+
+
+   }
+
+   RNA_apagar_rede(rede);
+   free(classe);
+   free(entrada_teste);
 
    return 0;
 }
